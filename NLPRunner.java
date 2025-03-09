b0f4496b34ef2e67a9f906451955100061db01c4
Camila E <cescamill0002@mymail.lausd.net>
2025-03-08 19:41:18 -0800

    Create NLPRunner.java

---
+++ NLPRunner.java
@@ -0,0 +1,13 @@
+public class NLPRunner {
+  public static void main(String[] args) {
+
+// Creates a new NLP object    
+NLP song = new NLP();
+
+// object calls prompt() method
+song.prompt();
+ 
+
+
+  }
+}
